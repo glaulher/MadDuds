416e71e97392aa3085e02e5976993e8083ce3ec3
glaulher ramalho medeiros <glaulher@glaulher.debian>
2020-04-26 22:21:17 -0300

    New background_parallax, jump_double, clean code script MadDuds.gd

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -8,10 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_process(true)
 #	get_node("Audio_cena1").play()	
+	
+	pass
+func set_process(true):
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	$Audio_cena1.play()
-	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
